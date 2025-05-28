d1ac8edb14b24f7432d962788c982e82e4e390ae
HHectorAlonso <Hector_alonso_martin@hotmail.es>
2025-05-28 18:19:50 +0000

    guardar codigo nuevo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 
-//window.onload = function() {
-/*una vez lo tenia "terminado", he querido probar a poner un boton que genere una nueva carta,
-  para esto dejo el window.onload en comentado*/
+//una vez lo tenia "terminado", he querido probar a poner un boton que genere una nueva carta,
+
 
 //cremaos el queryselector para el boton, con la funcion de click 
-document.querySelector(".boton").addEventListener("click", () => {  
-//creamos los arays y las variables para los aleatorios
+function generadorCartas(){
+  //creamos los arays y las variables para los aleatorios
     let numeroCarta = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
     let tipoCarta = ["♦", "♥", "♠", "♣"];
       let numeroAleatorio = Math.floor(Math.random()*numeroCarta.length);
@@ -24,6 +23,21 @@ document.querySelector(".boton").addEventListener("click", () => {
             tipo.textContent = tipoCarta[tipoAleatorio];  
             tipo.style.color = (tipo.textContent== "♦" || tipo.textContent== "♥")? "red" : "black";
          }) 
-    })
- // }
- //esta es la llave de cierre del window.onload, no lo he quitado, solo lo dejo comentado
+}
+/*window.onload = function() {
+    generadorCartas();
+ }*/
+
+//una vez lo tenia "terminado", he querido probar a poner un boton que genere una nueva carta,
+//cremaos el queryselector para el boton, con la funcion de click 
+/* document.querySelector(".boton").addEventListener("click", () => {  
+   generadorCartas();
+ })*/
+
+   //de ultimas, para probar el intervalo, he crado esta parte, dejando todo lo demas comentado
+   //si esto esta activo, no funciona el boton
+ setInterval(() => {
+  generadorCartas();
+ },500);
+
+ 
